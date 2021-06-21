2bec3549b8901b2e799d5f7911cb91b9f26b21e0
imform-mm-2112 <hojung@imform-mm-2112ui-Macmini.local>
2021-06-21 16:58:27 +0900

    중간업로드

--- src/containers/signup/index.tsx
+++ src/containers/signup/index.tsx
@@ -2,6 +2,9 @@ import { SignupForm } from 'components';
 import { MainLayout } from 'layouts';
 
 export const Signup = () => {
+  const a = function () {
+    console.log('cc');
+  };
   return (
     <MainLayout>
       <SignupForm />
