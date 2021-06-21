b2a903d4d80b24df520f3bb77b3ef5a3f6a7df83
imform-mm-2112 <hojung@imform-mm-2112ui-Macmini.local>
2021-06-21 16:55:07 +0900

    중간업로드

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,14 +1,9 @@
 import { LoginForm } from 'components';
 
 export const Login = () => {
-  const hanldeSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(e.target.value);
-  };
-
   return (
     <>
-      <LoginForm onSubmit={hanldeSubmit} />
+      <LoginForm />
     </>
   );
 };
