d97d8736cc6c22de103ebca27c6ed8cbacb37784
imform-mm-2112 <hojung@imform-mm-2112ui-Macmini.local>
2021-06-22 09:09:20 +0900

    husky

--- src/containers/signup/index.tsx
+++ src/containers/signup/index.tsx
@@ -2,9 +2,6 @@ import { SignupForm } from 'components';
 import { MainLayout } from 'layouts';
 
 export const Signup = () => {
-  const a = function () {
-    console.log('cc');
-  };
   return (
     <MainLayout>
       <SignupForm />
